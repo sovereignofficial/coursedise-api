bc2ef1e956860e63189a391cd273825a456fb259
rogueofficiale <egemenvkdvn35@gmail.com>
2023-07-27 15:45:35 +0200

    removed dist

--- dist/routes/root.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.root = void 0;
-function root(request, response) {
-    response.status(200).send("<h1> Express server is running.!");
-}
-exports.root = root;
